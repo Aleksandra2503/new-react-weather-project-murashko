2eb48b5fdbb7d276e5c76b1c26e458cebe723654
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-21 22:38:15 +0100

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,8 @@
 export default class Api {
   static url = "https://api.openweathermap.org";
-  static key = "5f472b7acba333cd8a035ea85a0d4d4c";
+  static key = "6f2c8d8e771e0ede0a15520e397b527c";
 }
+
+
+// Другой ( не твой )
+// static key = "5f472b7acba333cd8a035ea85a0d4d4c";
